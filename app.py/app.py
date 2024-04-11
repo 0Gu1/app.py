8cd1d543437c22794a1ae4c59c51c8788bc0bf23
0Gu1 <162440276+0Gu1@users.noreply.github.com>
2024-04-11 08:39:53 -0300

    Add files via upload

--- app.py/app.py
+++ app.py/app.py
@@ -1,9 +1,9 @@
 import os
 
-restaurantes = ['Pizza','VSFD'] #Lista de nomes de restaurantes
+restaurantes = ['Pizza','NAM'] #Lista de nomes de restaurantes
 
 def exibir_nome_do_programa():
-    print(""" VSFD = Vacas Saborosas & Farinhas Douradas = 
+    print(""" NAM = 
     """)
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
@@ -18,27 +18,53 @@ def voltar_ao_menu_principal():
     input('\n Digite a tecla "Enter" para voltar ao menu principal')    
     main()
 
-
-
-opcao_escolhida = int(input('Escolha uma opção: '))
-#print('Você escolheu a opção', opcao_escolhida)
-#print(f'Você escolheu a opção {opcao_escolhida}')
+def opcao_invalida():
+    print('Opção inválida!\n')
+    voltar_ao_menu_principal()
 
 def exibir_subtitulo(texto):
-    os.system('clear') #os.system('clear')
+    os.system('clear')
     print(texto)
     print()
 
+def exibir_subtitulo(texto):
+    os.system('clear')
+    print(texto)
+    print()
+
+def cadastrar_novo_restaurante():
+   exibir_subtitulo('Cadastro do novorestaurante: ')
+   nome_do_restaurante  = input('Digite o nome do restaurante')
+   restaurantes.append(nome_do_restaurante)
+   print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+   voltar_ao_menu_principal()  
+
+def listar_restaurante():
+    exibir_subtitulo('Listando os restaurantes: ')
+
+    for restaurante in restaurantes:
+        print(f'*{restaurante}')
+
+    voltar_ao_menu_principal()
+
+
 def escolher_opcao():
+ try:
+    opcao_escolhida = int(input('Escolha uma opção: '))
 
     if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
+        cadastrar_novo_restaurante()
     elif opcao_escolhida == 2:
-        print('Listar restaurantes')
+        listar_restaurante()
     elif opcao_escolhida == 3:
         print('Ativar restaurantes')
-    else:
+    elif opcao_escolhida ==4:
         finaliza_app()
+    else:
+        opcao_invalida()
+
+ except:
+    opcao_invalida()
 
 def main():
     os.system('clear')
