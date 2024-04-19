feffcb3998f76101f60cc8ccdb05778c3bd485aa
0Gu1 <162440276+0Gu1@users.noreply.github.com>
2024-04-19 12:04:50 -0300

    Add files via upload

--- app.py/app.py
+++ app.py/app.py
@@ -3,8 +3,7 @@ import os
 restaurantes = [{'nome':'restaurante NAM','categoria':'Alimento','ativo':False},
                 {'nome':'Santa','categoria':'carne','ativo':True},
                 {'nome':'CWB','categoria':'Sushi','ativo':False}
-                ] #Lista de nomes de restaurantes
-
+                ]
 def exibir_nome_do_programa():
     print(""" NAM = 
     """)
@@ -36,7 +35,7 @@ def exibir_subtitulo(texto):
 def cadastrar_novo_restaurante():
    exibir_subtitulo('Cadastro do novo restaurante: ')
    nome_do_restaurante  = input('Digite o nome do restaurante: ')
-   categoria = input(f'Digite a categoria do restaurante{nome_do_restaurante}: ')
+   categoria = input(f'Digite a categoria do restaurante {nome_do_restaurante}: ')
    dado_do_restaurante ={'nome':nome_do_restaurante,'categoria':categoria,'ativo':False}
    restaurantes.append(dado_do_restaurante)
    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
@@ -54,21 +53,36 @@ def listar_restaurante():
 
     voltar_ao_menu_principal()
 
+def alternar_estado_restaurante():
+   exibir_subtitulo('Alternando estado do restaurante')
+   nome_restaurante = input('Digite o nome do restaurante que deseja alterar o estado: ')
+   restaurante_encontrado = False
+
+   for restaurante in restaurantes:
+      if nome_restaurante == restaurante['nome']:
+         restaurante_encontrado = True
+         restaurante['ativo'] = not restaurante['nome']
+         mensagem = f'O restaurante {nome_restaurante} foi ativo com sucesso' if restaurante ['ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso'
+         print(mensagem)
+
+   if not restaurante_encontrado:
+      print('O restaurante não foi encontrado')
+   voltar_ao_menu_principal()
 
 def escolher_opcao():
  try:
-    opcao_escolhida = int(input('Escolha uma opção: '))
+     opcao_escolhida = int(input('Escolha uma opção: '))
 
-    if opcao_escolhida == 1:
-        cadastrar_novo_restaurante()
-    elif opcao_escolhida == 2:
-        listar_restaurante()
-    elif opcao_escolhida == 3:
-        print('Ativar restaurantes')
-    elif opcao_escolhida ==4:
-        finaliza_app()
-    else:
-        opcao_invalida()
+     if opcao_escolhida == 1:
+         cadastrar_novo_restaurante()
+     elif opcao_escolhida == 2:
+         listar_restaurante()
+     elif opcao_escolhida == 3:
+         alternar_estado_restaurante()
+     elif opcao_escolhida ==4:
+         finaliza_app()
+     else:
+         opcao_invalida()
 
  except:
     opcao_invalida()
