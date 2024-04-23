c18514f9b5f3ee2c9b50af3bb67302ae82a41c78
0Gu1 <162440276+0Gu1@users.noreply.github.com>
2024-04-23 09:51:43 -0300

    Add files via upload

--- app.py/app.py
+++ app.py/app.py
@@ -5,26 +5,43 @@ restaurantes = [{'nome':'restaurante NAM','categoria':'Alimento','ativo':False},
                 {'nome':'CWB','categoria':'Sushi','ativo':False}
                 ]
 def exibir_nome_do_programa():
+    '''Exibe o nome do restaurante na tela
+    '''
     print(""" NAM = 
     """)
 def exibir_opcoes():
+    '''Mostra as opções que podem ser escolhidas para cadastrar o restaurante
+    '''
     print('1. Cadastrar restaurante')
     print('2. Listar restaurante')
     print('3. Ativar restaurante')
     print('4. Sair')
 
 def finaliza_app():
+    '''Serve para finalizar o cadastro
+    '''
     exibir_subtitulo('Finalizar app')
 
 def voltar_ao_menu_principal():
+    '''Esta função serve para voltar ao menu do site
+    Input:
+    - Tecla "Enter" para voltar
+
+    Output:
+    -Volta ao menu principal
+    '''
     input('\n Digite a tecla "Enter" para voltar ao menu principal')    
     main()
 
 def opcao_invalida():
+    '''Se algo estiver errado mostrará o texto na tela
+    '''
     print('Opção inválida!\n')
     voltar_ao_menu_principal()
 
 def exibir_subtitulo(texto):
+     '''Deixa o nome mais bonito
+     '''
      os.system('clear')
      linha = '=' * (len(texto))
      print(linha)
@@ -33,6 +50,14 @@ def exibir_subtitulo(texto):
      print()
 
 def cadastrar_novo_restaurante():
+   '''Essa função é responsável por cadastrar um novo restaurante
+   Inputs:
+   - Nome do restaurante
+   - Categoria
+
+   Output:
+   - Adiciona um novo restaurante a lista de restaurantes
+   '''
    exibir_subtitulo('Cadastro do novo restaurante: ')
    nome_do_restaurante  = input('Digite o nome do restaurante: ')
    categoria = input(f'Digite a categoria do restaurante {nome_do_restaurante}: ')
@@ -42,6 +67,8 @@ def cadastrar_novo_restaurante():
    voltar_ao_menu_principal()
 
 def listar_restaurante():
+    '''Esta função é responsável por mostrar a lista e categoria de restaurantes cadastrados
+    '''
     exibir_subtitulo('Listando os restaurantes: ')
 
     print(f'{"Nome do restaurante".ljust(22)} | {"Categoria".ljust(20)} | Status')
@@ -54,6 +81,12 @@ def listar_restaurante():
     voltar_ao_menu_principal()
 
 def alternar_estado_restaurante():
+   '''Esta função anterna o estado do restaurante entre ativado e desativado
+   Input:
+   - Nome do restaurante que deseja alterar o estado
+
+   Output: Alterna entre ativado e desativado
+   '''
    exibir_subtitulo('Alternando estado do restaurante')
    nome_restaurante = input('Digite o nome do restaurante que deseja alterar o estado: ')
    restaurante_encontrado = False
@@ -70,6 +103,8 @@ def alternar_estado_restaurante():
    voltar_ao_menu_principal()
 
 def escolher_opcao():
+ '''Serve para escolher qual opção você quer
+ '''
  try:
      opcao_escolhida = int(input('Escolha uma opção: '))
 
